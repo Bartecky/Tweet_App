f37b53d6f5611a9de3439acd4de26bd3d5421170
Bartłomiej Borecki <borecki.bartek@gmail.com>
2018-12-30 00:16:54 +0100

    added user profile and followers feature

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followed_by', blank=True)
+
+    def __str__(self):
+        return str(self.following.all().count())
+
