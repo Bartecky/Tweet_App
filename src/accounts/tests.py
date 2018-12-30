a7c4a462c1376451c48291f819e6ff449949b1c0
Bartłomiej Borecki <borecki.bartek@gmail.com>
2018-12-30 02:36:38 +0100

    added feature (signal), that by creating user also creating user-profile. created tests

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import UserProfile
+from django.core.exceptions import FieldError
 
 # Create your tests here.
+
+User = get_user_model()
+
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        self.username = "some_user"
+        new_user = User.objects.create(username=self.username)
+
+    def test_profile_created(self):
+        user_profile = UserProfile.objects.filter(user__username=self.username)
+        self.assertTrue(user_profile.exists())
+        self.assertTrue(user_profile.count() == 1)
--- src/twitter_app/tests.py
+++ src/twitter_app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Tweet
 
-# Create your tests here.
+
+User = get_user_model()
+
+class TweetModelTestCase(TestCase):
+    def setUp(self):
+        random_user = User.objects.create(username='barteckyyy')
+
+    def test_tweet_item(self):
+        obj = Tweet.objects.create(
+            user=User.objects.first(),
+            content='random content'
+        )
+        self.assertTrue(obj.content == 'random content')
+        self.assertTrue(obj.id == 1)
+        absolute_url = reverse('tweet:tweet-detail', kwargs={'pk': 1})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
+
+    def test_tweet_url(self):
+        obj = Tweet.objects.create(
+            user=User.objects.first(),
+            content='random content'
+        )
+        absolute_url = reverse('tweet:tweet-detail', kwargs={'pk': obj.pk})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
